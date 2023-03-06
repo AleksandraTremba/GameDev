cf152582feb3c2b12670d606e31dd28f9f02f8ca
Jaroslavna Khorosheva <jakhor@ttu.ee>
2023-03-06 18:04:50 +0200

    mistakes of the past

--- client/core/src/com/mygdx/game/MyGdxGame.java
+++ client/core/src/com/mygdx/game/MyGdxGame.java
@@ -7,14 +7,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Screens.PlayScreen;
-import com.mygdx.game.States.GameStateManager;
-import com.mygdx.game.States.MenuState;
+
 
 public class MyGdxGame extends Game {
 	public static final int V_WIDTH = 400;
 	public static final int V_HEIGHT = 208;
 	public static final float PPM = 100;
-	private GameStateManager gsm;
 	public SpriteBatch batch;
 	// Texture img;
 	
@@ -23,15 +21,13 @@ public class MyGdxGame extends Game {
 		batch = new SpriteBatch();
 		setScreen(new PlayScreen(this));
 		// img = new Texture("frog1.png");
-		gsm = new GameStateManager();
-		gsm.push(new MenuState(gsm));
+
 	}
 
 	@Override
 	public void render () {
 		super.render();
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
+
 		//ScreenUtils.clear(1, 0, 0, 1);
 		//.begin();
 		//batch.draw(img, 0, 0);
