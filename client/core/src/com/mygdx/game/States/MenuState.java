46f8373146068b4461e9ad78dae400583c2ba89a
Aleksandra Tremba <aleksandra.tremba@gmail.com>
2023-03-06 16:47:06 +0200

    Adding free type font generator

--- client/core/src/com/mygdx/game/States/MenuState.java
+++ client/core/src/com/mygdx/game/States/MenuState.java
@@ -1,17 +1,33 @@
 package com.mygdx.game.States;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.mygdx.game.MyGdxGame;
 
 public class MenuState extends State{
     private Texture background;
     private Texture playButton;
+
+    private BitmapFont titleFont;
+    private String title = "Around the world";
+
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
         background = new Texture("mainmenufrog.jpg");
         playButton = new Texture("playbutton.jpg");
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(
+                Gdx.files.internal("fonts/Oswald-Bold.ttf")
+        );
+        FreeTypeFontGenerator.FreeTypeFontParameter paramsTitle = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        paramsTitle.size = 56;
+        paramsTitle.color = Color.BLACK;
+        titleFont = gen.generateFont(paramsTitle);
     }
 
     @Override
