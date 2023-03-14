97445b8d485f5c76dcb1f632094a8045365587c5
jakhor <jakhor@ttu.ee>
2023-03-14 20:09:03 +0000

    Upload New File

---
+++ client/assets/field-improved.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="field-improved" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="170" columns="17">
+ <image source="field-improved.png" width="320" height="192"/>
+</tileset>
